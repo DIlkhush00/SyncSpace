8eda8d3b556bd6ba02af44a6a738f8bcc5c32dd8
Dilkhush Purohit <dilkhushpurohit01@gmail.com>
2023-11-25 20:49:17 +0530

    Initial Project Setup

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import '../styles/App.css'
+
+function App() {
+
+  return (
+    <div className="w-screen h-screen bg-purple-800">
+      <h1 className="py-[20%] text-center text-white text-3xl font-bold underline">
+        Everything is working fine!
+      </h1>
+    </div>
+  )
+}
+
+export default App
